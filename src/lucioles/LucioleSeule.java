b0db412f902728375a585d1aed3c8dbb5070e3ae
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2022-11-18 16:10:17 +0100

    modif exo 3

--- src/lucioles/LucioleSeule.java
+++ src/lucioles/LucioleSeule.java
@@ -1,6 +1,7 @@
 package lucioles;
 
 
+//petit test 
 // Étape 1 : Simulation d'une seule luciole
 
 public class LucioleSeule {
@@ -34,19 +35,47 @@ public class LucioleSeule {
 
     public static double incrementeLuciole(double niveauEnergie, double deltaEnergie)
     {
-        return niveauEnergie + deltaEnergie;
+        niveauEnergie += deltaEnergie;
+        return niveauEnergie;
     }
-    public static void main(String[] args) {
-        //aleatoire entre 0 et 100
+
+
+
+
+    /* 
+    Cr´eer une fonction simuleLucioleNbPas qui initialise al´eatoirement une
+luciole, et fait ´evoluer cette luciole un certain nombre de fois (fourni en param`etre), en
+affichant la luciole `a chaque pas de temps, en mode verbeux. En particulier, v´erifier que
+votre luciole ´emet un flash au bon moment, et que son niveau d’´energie revient bien `a 0
+avant d’augmenter `a nouveau. Vous testerez cette fonction dans la fonction main.
+ */
+    public static void simuleLucioleNbPas(int nbPas)
+    {
         double lucioleEnergie =  RandomGen.rGen.nextDouble() * 100;
-        //changement manuel de lucioleenergie
+        for(int i = 0; i < nbPas; i++)
+        {
+            afficheLuciole(lucioleEnergie, true);
+            lucioleEnergie = incrementeLuciole(lucioleEnergie, RandomGen.rGen.nextDouble() * 100 - 50);
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        double lucioleEnergie =  RandomGen.rGen.nextDouble() * 100;
+        double lucioleDeltaEnergie = RandomGen.rGen.nextDouble() * 1;
+
+
+        //changement manuel de lucioleEnergie
         // lucioleEnergie =  100;
         // afficheLuciole(lucioleEnergie, true);
         
-        double lucioleDeltaEnergie = RandomGen.rGen.nextDouble() * 1;
-        System.out.println("DeltaEnergie: " + lucioleDeltaEnergie);
-        afficheLuciole(lucioleEnergie, true);
-        afficheLuciole(incrementeLuciole(lucioleEnergie, lucioleDeltaEnergie), true);
+        // System.out.println("DeltaEnergie: " + lucioleDeltaEnergie);
+        // afficheLuciole(lucioleEnergie, true);
+        // afficheLuciole(incrementeLuciole(lucioleEnergie, lucioleDeltaEnergie), true);
+
+        simuleLucioleNbPas(10);
         
     }
     
